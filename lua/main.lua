dc6a86db08425175bc9d52d6bf5ae2434122e9c8
Nevyn Bengtsson <nevyn@alloverse.com>
2021-04-21 14:57:40 +0200

    Fix assets for icons for apps

--- lua/main.lua
+++ lua/main.lua
@@ -1,4 +1,6 @@
-local frame = require("frame")
+local vec3 = require("modules.vec3")
+local mat4 = require("modules.mat4")
+local Frame = require("frame")
 
 local client = Client(
     arg[2], 
@@ -13,7 +15,7 @@ assets = {
     quit = ui.Asset.File("images/quit.png"),
 }
 for _, n in ipairs(appNames) do 
-    table.insert(assets, ui.Asset.File("models/"..n..".glb"))
+    assets[n] = ui.Asset.File("models/"..n..".glb")
 end
 app.assetManager:add(assets)
 
@@ -22,23 +24,30 @@ function AppView:_init(bounds, appName)
     self:super(bounds)
     local iconAsset = assets[appName]
     self.icon = self:addSubview(
-        ui.Asset.View(iconAsset, ui.Bounds{size=bounds.size:copy()})
+        ui.Asset.View(iconAsset, 
+            ui.Bounds{size=bounds.size:copy()}
+                :move(0, 0, 0.05)
+        )
     )
+    self.icon.transform = mat4.scale(mat4.identity(), mat4.identity(), vec3(0.25, 0.25, 0.25))
+    self.icon.color = {0.5, 0.2, 0.5, 1.0}
     self.brick = self:addSubview(ui.Cube(
         ui.Bounds{size=bounds.size:copy()}
         :insetEdges(0.05, 0.05, 0.05, 0.05, 0.00, 0.05)
-        :move()
+        :move(0, 0, -0.05)
     ))
+    self.brick.color = {0.9, 0.4, 0.3, 0.3}
     self.label = self:addSubview(
         ui.Label{
             bounds= ui.Bounds(0, 0, 0,   bounds.size.width, 0.05, 0.01)
                 :move(0, -bounds.size.height/2 + 0.08, 0),
-            text= appName
+            text= appName,
+            color = {1, 1, 1, 1}
         }
     )
 end
 
-local mainView = Frame(ui.Bounds(0, 1.4, -2,   1.5, 0.8, 0.06), 0.03)
+local mainView = Frame(ui.Bounds(0, 1.6, -2,   1.5, 0.8, 0.06), 0.03)
 mainView.grabbable = true
 
 local titleLabel = mainView:addSubview(ui.Label{
