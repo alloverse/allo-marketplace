36b55bc0b116b9c62b74a8d4335f7573b14178e1
Patrik Sjöberg <voxxar@gmail.com>
2022-04-20 11:57:15 +0200

    Fix upgrade-assists
    - Bump the marketplace allo* instead of jukebox
    - don’t stop if a submo didn’t need a bump

--- scripts/upgrade-assists.sh
+++ scripts/upgrade-assists.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 
-echo "Usage: update assist in jukebox, then run this script."
+echo "Usage: update assist in marketplace, then run this script."
 echo "This script will bump copy assist, and bump alloui and allonet in all the apps,"
 echo "and make a commit."
 echo "IT WILL NOT PUSH THEM. Use the push-apps.sh script to push after verifying that"
 echo "all the apps work after bumping."
 
+pushd apps > /dev/null
 
 for APP in `ls`; do
     cd $APP
@@ -19,9 +20,7 @@ for APP in `ls`; do
     cd ..
 done
 
-cp apps/allo-jukebox/allo/assist allo/
-cp apps/allo-jukebox/allo/assist.lua allo/
-cp apps/allo-jukebox/allo/boot.lua allo/
+popd > /dev/null
 
 ./allo/assist upgrade
 cd allo/deps/alloui
@@ -33,16 +32,16 @@ cd ../../..
 pushd apps > /dev/null
 
 for APP in `ls`; do
-    cd $APP
     echo "Bumping assist, alloui and allonet in $APP"
+    cd $APP
     git checkout -f main
     git pull
     git submodule sync
     git submodule update --init --recursive
     git lfs pull
-    cp ../allo-jukebox/allo/assist allo/
-    cp ../allo-jukebox/allo/assist.lua allo/
-    cp ../allo-jukebox/allo/boot.lua allo/
+    cp ../../allo/assist allo/
+    cp ../../allo/assist.lua allo/
+    cp ../../allo/boot.lua allo/
     ./allo/assist upgrade
     cd allo/deps/alloui
     git checkout main
@@ -52,7 +51,7 @@ for APP in `ls`; do
     git add allo/allonet.lock
     git add allo/deps/alloui
     git add allo
-    git commit -m "bump allonet, alloui and assist"
+    git commit -m "bump allonet, alloui and assist" || true
     cd ..
     git add $APP
 done
